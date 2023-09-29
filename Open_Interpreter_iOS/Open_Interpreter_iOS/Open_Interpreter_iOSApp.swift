845d7ce9932fa89c6141a44dd89a6efc76cc05ec
Ted Liao <ekbiker@gmail.com>
2023-09-29 05:28:54 -0700

    Added XCode Project files
    Added XCode Project files

---
+++ Open_Interpreter_iOS/Open_Interpreter_iOS/Open_Interpreter_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Open_Interpreter_iOSApp.swift
+//  Open_Interpreter_iOS
+//
+//  Created by Ted Liao on 9/29/23.
+//
+
+import SwiftUI
+
+@main
+struct Open_Interpreter_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
